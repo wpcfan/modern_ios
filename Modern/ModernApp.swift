13178da2fc71c99d6e77065c3f1f99358cbf5820
Peng <wpcfan@gmail.com>
2022-12-30 15:02:28 +0800

    Initial Commit

---
+++ Modern/ModernApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernApp.swift
+//  Modern
+//
+//  Created by 王芃 on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct ModernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
